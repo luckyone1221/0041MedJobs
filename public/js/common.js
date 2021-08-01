a3243ac11de5fd92d1f4ad8360716e2cd70ca1a7
luckyone <luckyone1221@gmail.com>
2021-08-01 05:27:27 +0300

    made new main

--- public/js/common.js
+++ public/js/common.js
@@ -180,7 +180,7 @@ function eventHandler() {
 
 	var x = window.location.host;
 	let screenName;
-	screenName = '045.png';
+	screenName = '045-576.png';
 
 	if (screenName && x.includes("localhost:30")) {
 		document.body.insertAdjacentHTML("beforeend", "<div class=\"pixel-perfect\" style=\"background-image: url(screen/".concat(screenName, ");\"></div>"));
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -168,7 +168,7 @@ function eventHandler() {
 	// JSCCommon.CustomInputFile(); 
 	var x = window.location.host;
 	let screenName;
-	screenName = '045.png';
+	screenName = '045-576.png';
 	if (screenName && x.includes("localhost:30")) {
 		document.body.insertAdjacentHTML("beforeend", `<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
 	}
@@ -891,9 +891,6 @@ function eventHandler() {
 			$(this).toggleClass('active');
 		})
 	})
-
-
-
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
